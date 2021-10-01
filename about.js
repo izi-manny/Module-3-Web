75ab43cf33240a3d334dbe29043638000d058004
Emmanuel Izilov <em5750@gmail.com>
2021-10-01 11:51:46 -0400

    part 2 completed

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('The form has been successfully submitted!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+function picMouseover(evt){
+	alert("You must be a Master Debugger!")
+}
+
+let duckPic = document.querySelector('#duck-picture')
+
+duckPic.addEventListener('mouseover', picMouseover)
